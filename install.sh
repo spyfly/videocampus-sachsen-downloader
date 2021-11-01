372300cb4e14ce7fa9f02836916af8e61b4a1d8e
Sebastian Heiden <seb.heiden@gmail.com>
2021-11-01 15:05:19 +0100

    Fix install.sh
    Create ~/.local/bin path, if it doesn't exist yet

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 BIN_PATH=~/.local/bin/vcs-downloader
+mkdir ~/.local/bin
 echo $'#!/bin/bash\npython3 '$PWD'/vcs-downloader.py' > $BIN_PATH
 chmod +x $BIN_PATH 
 echo "Installed vcs-downloader successfully"

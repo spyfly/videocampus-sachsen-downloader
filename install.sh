0e5e19f0065c5fd4f9a49489a62ea52aafe019f9
Sebastian Heiden <seb.heiden@gmail.com>
2023-03-01 02:47:42 +0100

    Fix installer typo

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 BIN_PATH=~/.local/bin/vcs-downloader
 mkdir ~/.local
 mkdir ~/.local/bin
-echo $'#!/bin/bash\npython3 '$PWD'/vcs-downloader.py $1 $2./' > $BIN_PATH
+echo $'#!/bin/bash\npython3 '$PWD'/vcs-downloader.py $1 $2' > $BIN_PATH
 chmod +x $BIN_PATH 
 echo "Installed vcs-downloader successfully"
